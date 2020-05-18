6da2fc64fbbb67d0f9c3e098e343d25593b32eb7
Piotr20 <piotrpospiech00@gmail.com>
2020-05-18 12:03:21 +0200

    dfsdfds

---
+++ JS/frontpage.js
@@ -0,0 +1,3 @@
+const line_1 = document.getElementById('first object svg');
+const line_2 = document.getElementById('secend object svg');
+const line_3 = document.getElementById('third object svg');
